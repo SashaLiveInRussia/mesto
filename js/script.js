d373a7f0107762f43a6384609bd31408ee30f313
Aleksandra <nickshi3477@gmail.com>
2022-01-23 06:54:38 +0300

    popup

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-let popup = document.getElementsByClassName('popup');
-let profileEdit = document.getElementsByClassName('profile__edit');
+let popup = document.querySelector('.popup');
+let profileEdit = document.querySelector('.profile__edit');
 
 
 function popupOpen() {
-    popup.classList.add('.popup-open');
+    popup.classList.add('popup-open');
 }
 
 function popupClose() {
-    popup.classList.remove('.popup-open');
+    popup.classList.remove('popup-open');
 }
 
 profileEdit.addEventListener('click', popupOpen);
