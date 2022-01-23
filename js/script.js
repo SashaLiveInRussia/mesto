31d57163ea416cbd9ad8f25e05407c1093d3e23e
Aleksandra <nickshi3477@gmail.com>
2022-01-23 09:22:28 +0300

    popup

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 let popup = document.querySelector('.popup');
 let profileEdit = document.querySelector('.profile__edit');
+let popupClose = document.querySelector('.close-popup');
+let inputNameProfile = document.querySelector('.popup__field-name');
+let inputSubNameProfile = document.querySelector('.popup__field-sub-name');
+let buttonSave = document.querySelector('.button__save');
 
 
-function popupOpen() {
-    popup.classList.add('popup-open');
-}
+buttonSave.addEventListener('click',  function(e) {
+    document.querySelector('.profile__name').textContent = inputNameProfile.value;
+    document.querySelector('.profile__sub-name').textContent = inputSubNameProfile.value;
+    popup.classList.remove('popup__opened');
+})
 
-function popupClose() {
-    popup.classList.remove('popup-open');
-}
+profileEdit.addEventListener('click', function(e) {
+    popup.classList.add('popup__opened');
+    inputNameProfile.value = document.querySelector('.profile__name').textContent;
+    inputSubNameProfile.value = document.querySelector('.profile__sub-name').textContent;
+})
 
-profileEdit.addEventListener('click', popupOpen);
+popupCLose.addEventListener('click', function(e) {
+    popup.classList.remove('popup__opened');
+})
 
