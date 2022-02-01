3aa76e0b43e6c298b4ad9fcc3edcbdd8614e5711
SashaLiveInRussia <nickshi3477@gmail.com>
2022-02-01 13:26:36 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
 let popup = document.querySelector('.popup');
 let profileEdit = document.querySelector('.profile__edit');
-let popupClose = document.querySelector('.popup__close-popup');
+let closePopup = document.querySelector('.popup__close-popup');
 let inputNameProfile = document.querySelector('.popup__field_edit-name');
 let inputSubNameProfile = document.querySelector('.popup__field_edit-prof');
+let profilName = document.querySelector('.profile__name');
+let profilSubName = document.querySelector('.profile__sub-name')
+
 let buttonSave = document.querySelector('.popup__button-save');
 
+function popupOpen(e) {
+	popup.classList.add('popup__opened');
+	inputNameProfile.value = profilName.textContent;
+	inputSubNameProfile.value = profilSubName.textContent;
+}
 
-buttonSave.addEventListener('click',  function(e) {
-    e.preventDefault();
-    document.querySelector('.profile__name').textContent = inputNameProfile.value;
-    document.querySelector('.profile__sub-name').textContent = inputSubNameProfile.value;
-    popup.classList.remove('popup__opened');
-})
+function popupCLose(e) {
+	popup.classList.remove('popup__opened');
+}
 
-profileEdit.addEventListener('click', function(e) {
-    popup.classList.add('popup__opened');
-    inputNameProfile.value = document.querySelector('.profile__name').textContent;
-    inputSubNameProfile.value = document.querySelector('.profile__sub-name').textContent;
-})
+function formSubmitHandler(e) {
+	e.preventDefault();
+	profilName.textContent = inputNameProfile.value;
+	profilSubName.textContent = inputSubNameProfile.value;
+	popupCLose;
+}
+
+buttonSave.addEventListener('submit', formSubmitHandler);
+
+profileEdit.addEventListener('click', popupOpen);
+
+closePopup.addEventListener('click', popupCLose);
 
 popup.addEventListener('submit', formSubmitHandler); 
 
-popupCLose.addEventListener('click', function(e) {
-    popup.classList.remove('popup__opened');
-})
+
 
