9cfb978ad247f7c6ebaf92ba35a6e8a770623f54
SashaLiveInRussia <nickshi3477@gmail.com>
2022-04-18 16:57:53 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -4,50 +4,54 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path'); // подключаем path к конфигу вебпак
 
 module.exports = {
-    entry: {
-        main: './src/pages/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: ''
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                // применять это правило только к CSS-файлам
-                test: /\.css$/,
-                // при обработке этих файлов нужно использовать
-                // MiniCssExtractPlugin.loader и css-loader
-                use: [MiniCssExtractPlugin.loader, {
-                    loader: 'css-loader'
-                }]
-            },
-            {
-                // регулярное выражение, которое ищет все файлы с такими расширениями
-                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-                type: 'asset/resource'
-            },
-        ]
-    },
-    mode: 'development',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+	entry: {
+		main: './src/pages/index.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+		publicPath: ''
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				// применять это правило только к CSS-файлам
+				test: /\.css$/,
+				// при обработке этих файлов нужно использовать
+				// MiniCssExtractPlugin.loader и css-loader
+				use: [MiniCssExtractPlugin.loader, {
+					loader: 'css-loader',
+					// добавьте объект options
+					options: { importLoaders: 1 }
+				},
+					// Добавьте postcss-loader
+					'postcss-loader']
+			},
+			{
+				// регулярное выражение, которое ищет все файлы с такими расширениями
+				test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+				type: 'asset/resource'
+			},
+		]
+	},
+	mode: 'development',
+	devServer: {
+		static: path.resolve(__dirname, 'dist'), // путь, куда "смотрит" режим разработчика
+		compress: true, // это ускорит загрузку в режиме разработки
+		port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
 
-        open: true // сайт будет открываться сам при запуске npm run dev
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html' // путь к файлу index.html
-        }),
-        new CleanWebpackPlugin(), // использовали плагин
-        new MiniCssExtractPlugin(),
-    ]
+		open: true // сайт будет открываться сам при запуске npm run dev
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html' // путь к файлу index.html
+		}),
+		new CleanWebpackPlugin(), // использовали плагин
+		new MiniCssExtractPlugin(),
+	]
 }
